e8002b15874209d96f759acfa57502cedc35c626
Thomas Wessel <turpleturtle12@gmail.com>
2024-11-02 20:30:49 -0700

    Complete L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const button = document.createElement("button");
+button.innerText = "Click me";
+
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+
+document.body.appendChild(button);
